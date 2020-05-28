e9b2f3137a670430e0bea1ba4266aaf4ee471d14
meryemmoualim <58748621+meryemmoualim@users.noreply.github.com>
2020-05-28 07:44:53 +0100

    Changed main (OpenPageSec)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("OpenPage.fxml"));
-        primaryStage.setTitle("I-Conferences");
-        primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.setMaximized(true);
+        Parent root = FXMLLoader.load(getClass().getResource("OpenPageSec.fxml"));
+        primaryStage.setTitle("E-Conferences");
+        primaryStage.setScene(new Scene(root));
+        
         primaryStage.show();
     }
 
